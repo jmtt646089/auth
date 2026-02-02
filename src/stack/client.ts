d3480681f59ac610ea7cb43b238e886e11cd568f
jmtt646089 <jadeseala@gmail.com>
2026-02-02 22:26:40 +1300

    Update client.ts

--- src/stack/client.ts
+++ src/stack/client.ts
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 export const stackClientApp = new StackClientApp({
   // You should store these in environment variables
-  projectId: "c87c47e4-653e-4304-a7c1-a366b5600ba2",
-  publishableClientKey: "pck_z02hkdr8rb4p0xkhcr4jkjg53k0t48dpcpz9qhfec6zdg",
+  projectId: import.meta.env.VITE_STACK_PROJECT_ID,
+  publishableClientKey: import.meta.env.VITE_STACK_PUBLISHABLE_CLIENT_KEY,
   tokenStore: "cookie",
   redirectMethod: {
     useNavigate,
